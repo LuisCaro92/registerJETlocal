9b24327b172829f841d697e7c2d47be6d30b4c25
LuisCaro92 <luis.carosalgado1@gmail.com>
2023-05-03 00:43:23 +0000

    creado localmente

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,10 +1,10 @@
 
 const Home = () => {
     return (
-        <div className="container-fluid ">
+        <div className="home container-fluid ">
             <div className="d-flex justify-content-center mt-5 ">
                 <div className="d-flex p-2 align-items-center mt-5">
-                    <h1 className="display-1 bg-secondary bg-opacity-50 rounded-4 text-light mt-5">Esto es el Home del Ejercicio</h1>
+                    <h1 className="display-1 bg-secondary bg-opacity-50 rounded-4 text-light mt-5">Bienvenidos</h1>
                 </div>
             </div>
         </div>
--- src/views/Nav.js
+++ src/views/Nav.js
@@ -8,14 +8,14 @@ const Nav = ({user}) => {
     const navigate = useNavigate()
     return (
         <div>
-            <nav className="navbar navbar-expand-lg navbar-dark bg-primary bg-opacity-75">
+            <nav className="navbar navbar-expand-lg navbar-dark bg-warning bg-opacity-75">
                 <div className="container-fluid">
                     <Link to="/" className="navbar-brand">Navbar</Link>
                     <button className="navbar-toggler " type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon "></span>
                     </button>
-                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul className="navbar-nav  me-auto mb-2 mb-lg-0">
+                    <div className="collapse navbar-collapse ms-auto" id="navbarSupportedContent">
+                        <ul className="navbar-nav  ms-auto mb-2 mb-lg-0">
                             <li className="nav-item">
                                 <Link to="/register" className="nav-link" aria-current="page">Register</Link>
                             </li>
